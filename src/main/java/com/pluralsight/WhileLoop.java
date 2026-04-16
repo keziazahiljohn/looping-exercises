581f70916d670f77ed818bb34a5d7648d5981c4a
keziazahiljohn <keziazahiljohn@gmail.com>
2026-04-16 12:20:17 -0400

    Finished while loop

--- src/main/java/com/pluralsight/WhileLoop.java
+++ src/main/java/com/pluralsight/WhileLoop.java
@@ -1,4 +1,19 @@
 package com.pluralsight;
 
+import java.util.Scanner;
+
 public class WhileLoop {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("What do you love? ");
+        String love = scanner.nextLine();
+        System.out.println("okay watch :)");
+
+        int i = 0;
+        while (i < 5) {
+            i++;
+            System.out.println("I love " + love + "!");
+        }
+    }
 }
