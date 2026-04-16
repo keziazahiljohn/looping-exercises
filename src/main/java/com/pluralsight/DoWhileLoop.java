b3754b7534b0e0df3642a6e207b9584f20b227ba
keziazahiljohn <keziazahiljohn@gmail.com>
2026-04-16 12:20:38 -0400

    Finished do while loop

--- src/main/java/com/pluralsight/DoWhileLoop.java
+++ src/main/java/com/pluralsight/DoWhileLoop.java
@@ -3,5 +3,18 @@ package com.pluralsight;
 import java.util.Scanner;
 
 public class DoWhileLoop {
+    public static void main(String[] args) {
+        Scanner myScanner = new Scanner(System.in);
 
+        System.out.print("What do you love? ");
+        String love = myScanner.nextLine();
+        System.out.println("Watch this");
+
+        int i = 0;
+        do {
+            System.out.println("I love " + love + "!");
+            i++;
+        } while (i < 5);
+
+    }
 }
